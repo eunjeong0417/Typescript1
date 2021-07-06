ef287bc0ffffd83d892ba9cd9669b75b6913d751
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-06 18:33:15 +0900

    타입스크립트 기본 타입

---
+++ getting-started/index.ts
@@ -0,0 +1,18 @@
+function sum(a:number , b:number):number {
+    return a + b;
+}
+
+sum(3,5);
+
+const str = "hello";
+const a:string = "hello";
+//문자열 타입 선언
+
+//숫자 배열
+let arr:Array<number> = [1,2,3]
+let items: number[] = [1,2,3]
+//items도 arr과 같은 숫자 배열. 동일함
+
+//문자 배열
+let heroes:Array<string> = ["apple","mango", "orange"]
+//숫자만 들어갈 수 있는 배열
