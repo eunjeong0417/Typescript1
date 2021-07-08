e49b2f8a611f95d54b5d6e4990777cf2f54c20e1
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-08 19:15:25 +0900

    key of 제네릭 타입 제한

--- getting-started/generics.ts
+++ getting-started/generics.ts
@@ -1,18 +1,74 @@
-// function logText(text) {
+// function logText(text: string) {
 //     console.log(text);
 //     return text
 // }
 
-// logText();
+// logText('a');
+// logText(100);
+// logText(true);
 
 //제네릭
 //함수 호출 시점에 데이터 타입을 정의한다
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+
+// logText<string>('abc');
+
 function logText<T>(text: T):T {
     console.log(text);
     return text;
 }
 
-logText<string>('hello')
-logText('하이')
-logText(100)
+const hi = logText<string>('hello');
+logText<number>(10);
+const login = logText<boolean>(true)
 
+const email: { value: string; selected: boolean }[] {
+    
+}
+
+function createDropdownItem(item: { value: string; selected: boolean }) {
+    
+}
+
+// interface Dropdown {
+//     value: string;
+//     selected: boolean;
+// }
+
+// const prt: Dropdown = { value: 10, selected: false}
+
+//제네릭으로 선언하기
+
+// interface Dropdown<T> {
+//     value: T;
+//     selected: boolean;
+// }
+
+// const abc: Dropdown<string> = { value: 'greeting', selected: true };
+
+// const emails: Dropdown<string>[]
+// const login: DropdownItem<number>
+
+// interface LengthType {
+//     length: number;
+// }
+
+// function title<T extends LengthType> (text:T):T {
+//     text.length;
+//     return text;
+// }
+
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getItem<T extends keyof ShoppingItem>(item: T):T {
+    return item;
+}
+
+getItem("name");
